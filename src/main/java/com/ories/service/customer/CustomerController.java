85f7d79ed1f9aa8fb79cba1c663bd56cc912168f
wang-onx <onix.wang@gmail.com>
2023-03-10 10:38:56 +0100

    update controller with graphql

--- src/main/java/com/ories/service/customer/CustomerController.java
+++ src/main/java/com/ories/service/customer/CustomerController.java
@@ -1,5 +1,7 @@
 package com.ories.service.customer;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,16 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
-import java.util.Optional;
+import java.util.function.Supplier;
 
 @Controller
 @ResponseBody
 public class CustomerController {
 
     private final CustomerService service;
+    private final ObservationRegistry observationRegistry;
 
-    public CustomerController(CustomerService service) {
+
+    public CustomerController(CustomerService service, ObservationRegistry observationRegistry) {
         this.service = service;
+        this.observationRegistry = observationRegistry;
     }
 
     @RequestMapping("/customers")
@@ -28,7 +33,14 @@ public class CustomerController {
     Customer byName(@PathVariable String name){
 
         Assert.state(Character.isUpperCase(name.charAt(0)), "the name must start with a capital letter");
-        return this.service.byName(name);
+        return Observation.createNotStarted("byName", this.observationRegistry)
+                .observe(new Supplier<Customer>() {
+                    @Override
+                    public Customer get() {
+                        return service.byName(name);
+                    }
+                });
+
     }
 
 
